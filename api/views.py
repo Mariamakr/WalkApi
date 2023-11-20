38b1279e60b782968367ee2397d84d5671d6aec8
Mariamakr <makrygianni.maria@gmail.com>
2023-11-20 11:50:26 +0200

    Custom-method for testing

--- api/views.py
+++ api/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import  Place, Rating
 from .serializers import PlaceSerializer, RatingSerializer
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset= Place.objects.all()
     serializer_class = PlaceSerializer
+
+    @action(detail=True, methods=['POST'])
+    def rate_place(self,request, pk=None):
+        response = {'message': 'is working'}
+        return Response(response, status=status.HTTP_200_OK)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset= Rating.objects.all()
